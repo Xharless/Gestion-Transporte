fced1615f4cd63ad8119e79df0808c6c1b10be8f
Xharless <carlosarevalo1702@hotmail.com>
2024-12-21 00:18:52 -0300

    añadir rutas y datos entre rutas

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false, // Asegúrate de que esto esté configurado en false
+            webSecurity: false
+        },
+        icon: path.join(__dirname, 'IMG', 'autobus.png')
+    });
+    
+    mainWindow.loadFile('index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
