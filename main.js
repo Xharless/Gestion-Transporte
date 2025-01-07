edc3cadf30421eaf1a2057288811ab404b5cf72a
Xharless <carlosarevalo1702@hotmail.com>
2025-01-07 14:42:37 -0300

    actualizacion

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
-const { app, BrowserWindow } = require('electron');
+const express = require('express');
 const path = require('path');
 
-let mainWindow;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false, // Asegúrate de que esto esté configurado en false
-            webSecurity: false
-        },
-        icon: path.join(__dirname, 'IMG', 'autobus.png')
-    });
-    
-    mainWindow.loadFile('index.html');
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
-});
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
